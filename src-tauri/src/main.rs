af4a7b7b735f372c5831a33e910ad438d4e03902
Alex R <alexdrg06@gmail.com>
2026-02-16 00:41:41 -0300

    chore: professional branding and logic documentation overhaul

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,9 @@
+/**
+ * Author: Alejandro Ramírez
+ * Project: ClioBulk
+ * Logic: High-performance image processing application entry point (Tauri).
+ */
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
